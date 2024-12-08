d00425e9f9cab064a66633c5a0ea779b014bfddb
AnandKumar2002 <anandkumar101002@gmail.com>
2024-12-08 20:12:52 +0530

    pwa 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwaApp';
+  users: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchUsers();
+  }
+
+  fetchUsers(): void {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+    this.http.get<any[]>(apiUrl).subscribe({
+      next: (data) => {
+        console.log('User data fetched successfully:', data);
+        this.users = data;
+      },
+      error: (error) => {
+        console.error('Error fetching user details:', error);
+      },
+      complete: () => {
+        console.log('User data fetch completed.');
+      },
+    });
+  }
 }
